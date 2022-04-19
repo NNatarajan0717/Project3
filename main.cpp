5f6469f90c727edae5c24c303ba4658f6a340295
ASantana0924 <73971837+ASantana0924@users.noreply.github.com>
2022-04-18 23:30:13 -0400

    updated Main.cpp with selection screen
    Still needs finilization

--- main.cpp
+++ main.cpp
@@ -18,10 +18,12 @@ int main()
     getline(ratingsInfile, buffer);
 
     //map of vectors of movies, each vector contains movies of the same genre
-    map<string, vector<Movie_Vertex>> movieGenres;
+    //map<string, vector<Movie_Vertex>> movieGenres;
+
+    vector<Movie_Vertex> movieList;
 
     /*going through input files line by line, still have to create graph object in order to store movie vertices*/
-    for (int i = 0; i < 50; i++) 
+    for (int i = 0; i < 10; i++) 
     {
 
         //get index of movie
@@ -66,18 +68,84 @@ int main()
         Movie_Vertex movie(title, genres, stod(rating));
 
         //push movie object into vector of movies
-        movieGenres[movie.getGenre()].push_back(movie);
+        movieList.push_back(movie);
 
     }
 
-    map<string, Graph> movieGraphs;
+    Graph graph("", movieList);
 
-    for (auto itr = movieGenres.begin(); itr != movieGenres.end(); itr++) 
+    Graph* ptr = &graph;
+
+    //cout << graph.breadthDepthSearch(ptr, "The Ball Game")->getTitle();
+
+    //format for user input
+    cout << "Welcome to the Movie Carosel!\n\n";
+
+    bool movieFound = false;
+
+    while (!movieFound) 
     {
     
-        movieGraphs.insert(pair<string, Graph>(itr->first, Graph(itr->first, itr->second)));
-        cout << itr->first << " : " << to_string(itr->second.size()) << endl;
+        cout << "Choose an option to start:\n\nSearch by rating (enter: 1)\nSearch by genre (enter: 2)\nSearch by name (enter: 3)\nSearch for a random movie (enter: 4)\noption:";
+        
+        string searchChoice;
+       
+        getline(cin, searchChoice);
+
+        if (stoi(searchChoice) == 1) 
+        {
+        
+            string upperBound, lowerBound;
+            cout << "\nEnter Upperbound (0.0 - 10.0): ";
+            getline(cin, upperBound);
+            cout << "Enter Lowerbound (0.0 - 10.0): ";
+            getline(cin, lowerBound);
+            graph.findRatingBDS(ptr, stod(lowerBound), stod(upperBound));
+
+        }
+        else if (stoi(searchChoice) == 2) 
+        {
+
+            string genre;
+            cout << "\nEnter Genre: ";
+            getline(cin, genre);
+            graph.findGenreBDS(ptr, genre);
+
+        }
+        else if (stoi(searchChoice) == 3) 
+        {
+
+            cout << "\nEnter Movie Name:";
+            string movieTitle;
+            getline(cin, movieTitle);
+            graph.findTitleBDS(ptr, movieTitle);
+            
+        }
+        else if (stoi(searchChoice) == 4) 
+        {
+
+            graph.randomMovie(ptr);
+
+        }
+        else 
+        {
+        
+            cout << "\nInvalid Input!\n\n";
+
+        }
+
+        cout << "Is this the movie you wanted? (y / n)" << endl;
+        string answer;
+        getline(cin, answer);
+        if (answer == "y") 
+        {
+        
+            movieFound = true;
+
+        }
 
     }
+
+    return 0;
 
 }
